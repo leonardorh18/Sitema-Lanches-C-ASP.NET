eb294bbaa7dd67ca728d5980feb9d0d25aa96102
leonardorh18 <leoheiro@hotmail.com>
2022-04-07 16:40:21 -0300

    populando tabelas

---
+++ LanchesMac/Migrations/20220407192916_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaNome, Descricao) " +
+                "values ('Normal', 'Lanches feios com ingredientes normais.')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias (CategoriaNome, Descricao) " +
+                "values ('Natural', 'Lanches feios com ingredientes naturais.')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from categoria");
+        }
+    }
+}
